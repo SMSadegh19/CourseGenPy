7a92c37b6c55666f7883259d91c317cebd7f1645
M. Sadegh Salimi <sadeghclan93@gmail.com>
2023-08-28 06:38:43 +0330

    add Topics generation template

---
+++ src/Templates/TopicGenTemplate.py
@@ -0,0 +1,6 @@
+topic_generation_prompt = """We want to design a course with the following details:
+Title: {course_title}
+Target audience: {target_audience}
+
+Please suggest some topics for the whole course.
+"""
