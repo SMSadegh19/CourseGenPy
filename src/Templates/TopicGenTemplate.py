09abaa90b2ab9adba6f33c9921a364c453e89e13
M. Sadegh Salimi <sadeghclan93@gmail.com>
2023-08-28 07:07:13 +0330

    update TopicsGenTemplate

--- src/Templates/TopicGenTemplate.py
+++ src/Templates/TopicGenTemplate.py
@@ -1,6 +1,19 @@
-topic_generation_prompt = """We want to design a course with the following details:
+topics_parser = CommaSeparatedListOutputParser()
+topics_format_instructions = topics_parser.get_format_instructions()
+print(f'output format instruction: \n{topics_format_instructions}')
+
+
+topic_generation_prompt = """\
+We want to design a course with the following details:
 Title: {course_title}
 Target audience: {target_audience}
 
-Please suggest some topics for the whole course.
+Suggest some topics for the whole course in my requested format:
+{format_instructions}
 """
+
+topics_prompt_template = PromptTemplate(
+    template=topic_generation_prompt,
+    input_variables=['course_title', 'target_audience'],
+    partial_variables={"format_instructions": topics_format_instructions}
+)
