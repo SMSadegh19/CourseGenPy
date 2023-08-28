6355461c40cc97f7a28f2b6c003b58840dadf0b9
M. Sadegh Salimi <sadeghclan93@gmail.com>
2023-08-28 06:39:01 +0330

    add subtopics generation template

---
+++ src/Templates/SubTopicGenTemplate.py
@@ -0,0 +1,6 @@
+subtopic_generation_prompt = """We want to design a course with the following details:
+Title: {course_title}
+Target audience: {target_audience}
+
+Please suggest some subtopic for our topic with the name: {topic_name}
+"""
